b841ba8b49c3a1b8c84bf64eac150e147ac0f548
Kirill Kurentsov <immortalsteel@gmail.com>
2022-08-29 19:25:20 +0300

    Completed stylesheet and fixed result for Simple

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const colors = ["green", "red", "rgba(133, 122, 200)", "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
-btn.addEventListener("click", function() {
-    const randomNumber = getRandomNumber ();
-    console.log(randomNumber);
-    document.body.style.backgroundColor = colors[randomNumber];
-    color.textContent = color[randomNumber];
+btn.addEventListener("click", function () {
+  const randomNumber = getRandomNumber();
+  console.log(randomNumber);
+  console.log(colors[randomNumber]);
+  let printColor = "";
+  printColor += colors[randomNumber];
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = printColor;
 });
 
 function getRandomNumber() {
-    return Math.floor(Math.random() * colors.length);
+  return Math.floor(Math.random() * colors.length);
 }
